1922ac13f82ee6b7aeb1f9e4a4219c5faad50274
sajalsharma07 <sajalsharmaoffice@gmail.com>
2026-07-02 23:45:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const ShortUrl = require('./Models/db.shortUrl')
 require('dotenv').config();
+
 const app = express()
 
 mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("MongoDB error:", err));
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
@@ -16,7 +19,6 @@ app.get('/', async (req, res) => {
 
 app.post('/shortUrls', async (req, res) => {
   await ShortUrl.create({ full: req.body.fullUrl })
-
   res.redirect('/')
 })
 
@@ -25,9 +27,13 @@ app.get('/:shortUrl', async (req, res) => {
   if (shortUrl == null) return res.sendStatus(404)
 
   shortUrl.clicks++
-  shortUrl.save()
+  await shortUrl.save()
 
   res.redirect(shortUrl.full)
 })
 
-app.listen(process.env.PORT || 5000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
